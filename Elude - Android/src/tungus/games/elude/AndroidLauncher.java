f372b2cd140b3d681f24988a88ce56f7c3d436a7
Peti <pmernyei@gmail.com>
2014-03-13 20:20:22 +0100

    Finished renaming Probably requires Eclipse reimporting, cleaning, several Eclipse restarts etc...

--- Elude - Android/src/tungus/games/elude/AndroidLauncher.java
+++ Elude - Android/src/tungus/games/elude/AndroidLauncher.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-public class MainActivity extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
