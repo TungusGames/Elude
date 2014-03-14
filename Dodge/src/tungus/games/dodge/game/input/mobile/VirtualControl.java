2652db5ffb28f6106c8f8b0d71874325be132ec5
Tamás Tardos <tardostamas@t-online.hu>
2014-03-14 22:19:33 +0100

    A new kind of mobile control ("VirtualControl")- read below! - When you drag your fingers, it records the directional change - If you leave your finger in the same place, you continue in the same direction - You only stop if you lift your finger - Slow movements don't have effect yet... :(

---
+++ Dodge/src/tungus/games/dodge/game/input/mobile/VirtualControl.java
@@ -0,0 +1,59 @@
+package tungus.games.dodge.game.input.mobile;
+
+import tungus.games.dodge.game.input.Controls;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class VirtualControl implements Controls {
+
+	private final OrthographicCamera interfaceCamera;
+	
+	private Vector2 lastTouch = new Vector2();
+	private Vector2 lastV = new Vector2();
+	private Vector2 v = new Vector2();
+	private Vector2 v2 = new Vector2();
+	private Vector3 v3 = new Vector3();
+	
+	private boolean touched = false;
+	
+	private static final float MAX_SQUARED_DIFF = 0.1f;
+	
+	public VirtualControl(OrthographicCamera cam, float frustumWidth, float frustumHeight) {
+		this.interfaceCamera = cam;
+	}
+
+	@Override
+	public Vector2 getDir() {
+		if (!touched && Gdx.input.isTouched()) {
+			v3.set((float)Gdx.input.getX(), (float)Gdx.input.getY(), 0f);
+			interfaceCamera.unproject(v3);
+			touched = true;
+			lastTouch.set(v3.x, v3.y);
+			lastV.set(0, 0);
+			return v.set(lastV);
+		} else if (Gdx.input.isTouched()) {
+			v3.set((float)Gdx.input.getX(), (float)Gdx.input.getY(), 0f);
+			interfaceCamera.unproject(v3);
+			v.set(v3.x, v3.y);
+			v2.set(v).sub(lastTouch);
+			lastTouch.set(v);
+			touched = true;
+			if (v2.len2() > MAX_SQUARED_DIFF)
+				return v.set(lastV.set(v2.nor()));
+			else return v.set(lastV);
+		} else {
+			touched = false;
+			lastV.set(0, 0);
+			return v.set(lastV);
+		}
+				
+	}
+	
+	@Override
+	public void draw(SpriteBatch batch) {
+	}
+}
--- Dodge/src/tungus/games/dodge/screens/GameScreen.java
+++ Dodge/src/tungus/games/dodge/screens/GameScreen.java
@@ -9,7 +9,7 @@ import tungus.games.dodge.game.Vessel;
 import tungus.games.dodge.game.World;
 import tungus.games.dodge.game.input.Controls;
 import tungus.games.dodge.game.input.KeyControls;
-import tungus.games.dodge.game.input.mobile.DynamicDPad;
+import tungus.games.dodge.game.input.mobile.VirtualControl;
 
 import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Game;
@@ -64,7 +64,7 @@ public class GameScreen extends BaseScreen {
 			if (Gdx.app.getType() == ApplicationType.Desktop || Gdx.app.getType() == ApplicationType.WebGL) {
 				controls.add(new KeyControls(new int[] {Keys.W, Keys.A, Keys.S, Keys.D}));
 			} else {
-				controls.add(new DynamicDPad(interfaceCamera, FRUSTUM_WIDTH, FRUSTUM_HEIGHT));
+				controls.add(new VirtualControl(interfaceCamera, FRUSTUM_WIDTH, FRUSTUM_HEIGHT));
 			}				
 			dirs[i] = controls.get(i).getDir();
 
