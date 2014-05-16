e6b14c57cbf5d48c94bda3075104ffcc8668f103
szentsas <benedek.stadler@gmail.com>
2014-05-16 23:20:37 +0200

    New Levels!!!!
    Signed-off-by: szentsas <benedek.stadler@gmail.com>

--- Levelgen/src/elude/levelgen/Main.java
+++ Levelgen/src/elude/levelgen/Main.java
@@ -51,8 +51,8 @@ public class Main {
 				scores.add(new FiniteLevelScore(sc.nextFloat(), sc.nextFloat()));
 				hpDrop = sc.nextFloat();
 				speedDrop = sc.nextFloat();
-				wipeDrop = sc.nextFloat();
 				shieldDrop = sc.nextFloat();
+				wipeDrop = sc.nextFloat();
 				while (!sc.next().equals("wavestart"))
 					;
 				int t = sc.nextInt();
@@ -110,38 +110,12 @@ public class Main {
 			waves.clear();
 			levelNum++;
 		}
-		// Fill up scores to 50 levels
-		for (int i = levelNum; i <= 50; i++)
-			scores.add(new FiniteLevelScore(150, 50));
 		// Output score file
 		try {
 			FileOutputStream fileOut = new FileOutputStream("../Elude - Android/assets/medals/finite.medal");
 			ObjectOutputStream out;
 			out = new ObjectOutputStream(fileOut);
 			out.writeObject(scores);
-			out.close();
-		} catch (IOException e11) {
-			e11.printStackTrace();
-		}
-	}
-	
-	
-	
-	@Deprecated
-	public static void writeFiniteMedals() {
-		List<FiniteLevelScore> list = new ArrayList<>();
-		FiniteLevelScore medal = new FiniteLevelScore();
-		medal.completed = true;
-		medal.timeTaken = 150;
-		medal.hpLost = 50;
-		for (int i = 0; i < 50; i++) {
-			list.add(medal);
-		}
-		try {
-			FileOutputStream fileOut = new FileOutputStream("../Elude - Android/assets/medals/finite.medal");
-			ObjectOutputStream out;
-			out = new ObjectOutputStream(fileOut);
-			out.writeObject(list);
 			out.close();
 		} catch (IOException e11) {
 			e11.printStackTrace();
@@ -167,196 +141,4 @@ public class Main {
 			e11.printStackTrace();
 		}
 	}
-	
-	/*@Deprecated
-	public static void outputLevel(int num) {
-		FileOutputStream fileOut = null;
-		Level lvl = new Level();
-		lvl.waves = waves;
-		lvl.hpChance = hpDrop;
-		lvl.speedChance = speedDrop;
-		lvl.rocketWipeChance = wipeDrop;
-		lvl.shieldChance = shieldDrop;
-		try {
-			fileOut = new FileOutputStream(num+levelOffset + ".lvl");
-			ObjectOutputStream out;
-			out = new ObjectOutputStream(fileOut);
-			out.writeObject(lvl);
-			out.close();
-		} catch (IOException e11) {
-			e11.printStackTrace();
-		}
-		waves.clear();
-	}
-
-	@Deprecated
-	public static void level1() {
-		for (int i = 0; i < 5; i++) {
-			ArrayList<EnemyType> e = new ArrayList<EnemyType>();
-			for (int j = 0; j <= i; j++) {
-				e.add(EnemyType.STANDING);
-			}
-			Wave w = new Wave(-1, 0, e);
-			waves.add(w);
-		}
-		ArrayList<EnemyType> e = new ArrayList<EnemyType>();
-		e.add(EnemyType.MOVING);
-		Wave w = new Wave(-1, 0, e);
-		waves.add(w);
-		for (int i = 1; i < 3; i++) {
-			ArrayList<EnemyType> e1 = new ArrayList<EnemyType>();
-			for (int j = 0; j <= i; j++) {
-				e1.add(EnemyType.MOVING);
-			}
-			Wave w1 = new Wave(5, 0, e1);
-			waves.add(w1);
-		}
-		ArrayList<EnemyType> e1 = new ArrayList<EnemyType>();
-		e1.add(EnemyType.STANDING);
-		e1.add(EnemyType.MOVING);
-		e1.add(EnemyType.STANDING);
-		e1.add(EnemyType.MOVING);
-		Wave w1 = new Wave(-1, 2, e1);
-		waves.add(w1);
-		outputLevel(1);
-	}
-	
-	@Deprecated
-	public static void level2() {
-		for (int i = 0; i < 5; i++) {
-			List<EnemyType> l = new ArrayList<EnemyType>();
-			l.add(EnemyType.STANDING);
-			l.add(EnemyType.MOVING);
-			waves.add(new Wave(3, 0, l));
-		}
-		
-		List<EnemyType> l = new ArrayList<EnemyType>();
-		for (int i = 0; i < 8; i++)
-			l.add(EnemyType.STANDING);
-		waves.add(new Wave(-1, 0, l));
-		
-		l = new ArrayList<EnemyType>();
-		for (int i = 0; i < 8; i++)
-			l.add(EnemyType.MOVING);
-		waves.add(new Wave(-1, 0, l));
-		
-		l = new ArrayList<EnemyType>();
-		for (int i = 0; i < 8; i++) {
-			l.add(EnemyType.STANDING);
-			l.add(EnemyType.MOVING);
-		}
-		waves.add(new Wave(-1, 0, l));
-		outputLevel(2);
-	}
-	
-	@Deprecated
-	public static void level3() {
-		ArrayList<EnemyType> e = new ArrayList<EnemyType>();
-		e.add(EnemyType.MOVING);
-		e.add(EnemyType.MOVING);
-		e.add(EnemyType.MOVING);
-		Wave w = new Wave(-1, 0, e);
-		waves.add(w);
-		
-		e = new ArrayList<EnemyType>();
-		e.add(EnemyType.STANDING);
-		e.add(EnemyType.STANDING);
-		e.add(EnemyType.MOVING);
-		e.add(EnemyType.MOVING);
-		w = new Wave(7, 0, e);
-		waves.add(w);
-		
-		e = new ArrayList<EnemyType>();
-		e.add(EnemyType.KAMIKAZE);
-		e.add(EnemyType.KAMIKAZE);
-		w = new Wave(7, 3, e);
-		waves.add(w);
-		
-		e = new ArrayList<EnemyType>();
-		for (int i = 0; i < 4; i++)
-			e.add(EnemyType.MOVING);
-		w = new Wave(3, 0, e);
-		waves.add(w);
-		
-		e = new ArrayList<EnemyType>();
-		for (int i = 0; i < 8; i++)
-			e.add(EnemyType.KAMIKAZE);
-		w = new Wave(-1, 0, e);
-		waves.add(w);
-		
-		e = new ArrayList<EnemyType>();
-		for (int i = 0; i < 5; i++) {
-			e.add(EnemyType.KAMIKAZE);
-			e.add(EnemyType.MOVING);
-			e.add(EnemyType.STANDING);
-		}
-			
-		w = new Wave(-1, 3, e);
-		waves.add(w);
-		outputLevel(3);
-	}
-
-	@Deprecated
-	public static void level4() {
-		ArrayList<EnemyType> e = new ArrayList<>();
-		e.add(EnemyType.MOVING);
-		e.add(EnemyType.MOVING);
-		e.add(EnemyType.MOVING);
-		Wave w = new Wave(-1, 0, e);
-		waves.add(w);
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.MOVING);
-		e.add(EnemyType.MOVING);
-		e.add(EnemyType.MOVING);
-		waves.add(new Wave(10, 2, e));
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.STANDING_FAST);
-		waves.add(new Wave(-1, 0, e));
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.STANDING_FAST);
-		e.add(EnemyType.STANDING_FAST);
-		e.add(EnemyType.STANDING_FAST);
-		waves.add(new Wave(-1, 0, e));
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.MOVING_MATRIX);
-		waves.add(new Wave(-1, 0, e));
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.MOVING_MATRIX);
-		e.add(EnemyType.MOVING_MATRIX);
-		e.add(EnemyType.MOVING_MATRIX);
-		waves.add(new Wave(-1, 0, e));
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.MOVING_MATRIX);
-		e.add(EnemyType.MOVING_MATRIX);
-		e.add(EnemyType.STANDING_FAST);
-		e.add(EnemyType.STANDING_FAST);
-		waves.add(new Wave(-1, 0, e));
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.MOVING_MATRIX);
-		e.add(EnemyType.STANDING_FAST);
-		waves.add(new Wave(8, 2, e));
-		
-		e = new ArrayList<>();
-		e.add(EnemyType.MOVING_MATRIX);
-		e.add(EnemyType.STANDING_FAST);
-		waves.add(new Wave(8, 2, e));
-		
-		e = new ArrayList<>();
-		for (int i = 0; i < 3; i++) {
-			e.add(EnemyType.STANDING);
-			e.add(EnemyType.STANDING_FAST);
-			e.add(EnemyType.MOVING);
-			e.add(EnemyType.MOVING_MATRIX);
-		}
-		waves.add(new Wave(-1, 0, e));
-		
-		outputLevel(4);
-	}*/
 }
