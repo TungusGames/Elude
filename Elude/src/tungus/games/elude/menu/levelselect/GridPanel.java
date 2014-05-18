a087a031aa3455e18aeddafbc0b9f55f9d9e0ec7
Peti <pmernyei@gmail.com>
2014-05-18 21:45:08 +0200

    HOTFIX - All levels were unlocked for screenshotting...

--- Elude/src/tungus/games/elude/menu/levelselect/GridPanel.java
+++ Elude/src/tungus/games/elude/menu/levelselect/GridPanel.java
@@ -94,7 +94,6 @@ public class GridPanel {
 			} else {
 				open = (ScoreData.totalStars >= (i+1)*10);
 			}
-			open = true; ///DEBUG!!
 			buttons[i] = new LevelButton(i, finite, open);
 			buttons[i].setBounds(buttonTouchAreas[i%visibleButtons].x, buttonTouchAreas[i%visibleButtons].y, BUTTON_DRAW_SIZE, BUTTON_DRAW_SIZE);
 			buttons[i].setOrigin(BUTTON_DRAW_SIZE/2, BUTTON_DRAW_SIZE/2);
