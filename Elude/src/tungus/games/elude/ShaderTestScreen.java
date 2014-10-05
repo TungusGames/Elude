da4f5b721b81e27e44893ae745437a2cd5e52999
Peti <pmernyei@gmail.com>
2014-10-05 20:46:19 +0200

    Shader working pretty well

--- Elude/src/tungus/games/elude/ShaderTestScreen.java
+++ Elude/src/tungus/games/elude/ShaderTestScreen.java
@@ -14,17 +14,24 @@ public class ShaderTestScreen extends BaseScreen {
 	private final SpriteBatch batch;
 	private final OrthographicCamera cam;
 	
-	private float time = 0;
+	
+	private float[] time = new float[]{0.1f, 0.2f, 1.5f, 1.56f};
+	private float[] centers = new float[]{10, 6, 12, 10, 4, 8, 16, 5};
 	
 	public ShaderTestScreen(Game game) {
 		super(game);
 		shader = new ShaderProgram(Gdx.files.internal("shaders/minevertex"),
 								   Gdx.files.internal("shaders/minefragment"));
 		Gdx.app.log("Shader", ""+shader.getLog());
+		shader.begin();
 		shader.setUniformf("worldSize", 20, 12);
 		shader.setUniformf("viewportSize", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		shader.setUniformf("center", 10, 6);
-		shader.setUniformf("R", 4);
+		shader.setUniformf("R", 2);
+		shader.setUniform2fv("center[0]", centers, 0, centers.length);
+		shader.setUniform1fv("time[0]", time, 0, time.length);
+		shader.setUniformi("L", 4);
+		shader.end();
 		cam = ViewportHelper.newCamera(20, 12);
 		batch = new SpriteBatch();
 		batch.setProjectionMatrix(cam.combined);
@@ -35,13 +42,12 @@ public class ShaderTestScreen extends BaseScreen {
 	@Override
 	public void render(float deltaTime)
 	{
-		time += deltaTime;
+		for (int i = 0; i < time.length; i++) {
+			time[i] += deltaTime;
+		}
 		batch.begin();
-		shader.setUniformf("time", time);
-		shader.setUniformf("worldSize", 20, 12);
-		shader.setUniformf("viewportSize", Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		shader.setUniformf("center", 10, 6);
-		shader.setUniformf("R", 4);
+		//shader.setUniformf("time", time);
+		shader.setUniform1fv("time[0]", time, 0, time.length);
 		batch.draw(Assets.whiteRectangle, 0, 0, 20, 12);
 		batch.end();
 	}
